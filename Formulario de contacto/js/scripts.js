f9c065a63a78945ee75cc2726bbb4e76fd197fef
Sofía Latvenas <129110254+SofiLatvenas96@users.noreply.github.com>
2023-04-14 12:24:53 -0300

    Formulario de contacto
    Está hecho con HTML, CSS Bootstrap y JavaScript. Tiene validación de campos y envía el mensaje a una casilla de correo elegida.

---
+++ Formulario de contacto/js/scripts.js
@@ -0,0 +1,11 @@
+//Dos caminos
+
+function enviarFormulario() {
+  var formulario = document.getElementById("contactSection");
+  var datos = new FormData(formulario);
+  
+  // Validación de formulario
+  var xhr1 = new XMLHttpRequest();
+  xhr1.open("POST", "/procesar");
+  xhr1.send(datos);
+}  
